2576af0a113b84fbf83733f685e9ccbce215fef4
Ansh Tyagi <anshtyagi10501@gmail.com>
2025-12-24 14:38:40 +0530

    initialize java app

---
+++ src/main/java/in/dev_shiv_ansh/trajectory_backend/TrajectoryBackendApplication.java
@@ -0,0 +1,13 @@
+package in.dev_shiv_ansh.trajectory_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrajectoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrajectoryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dev_shiv_ansh/trajectory_backend/TrajectoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dev_shiv_ansh.trajectory_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrajectoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
